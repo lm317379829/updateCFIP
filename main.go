5fcf3a558cb082573222a98687068ff832639504
lm317379829 <lm317379829@163.com>
2025-06-21 07:31:38 +0800

    fix bug

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -25,24 +26,48 @@ type Config struct {
 	} `json:"telegram,omitempty"`
 }
 
-func retryRequest(method, url string, body *bytes.Buffer, header map[string]string) (*http.Response, error) {
+type ZoneInfos struct {
+	Result []struct {
+		ID string `json:"id"`
+	} `json:"result"`
+}
+
+type DNSInfos struct {
+	Result []struct {
+		ID       string `json:"id"`
+		Type     string `json:"type"`
+		RemortIP string `json:"content"`
+		Proxy    bool   `json:"proxied"`
+	} `json:"result"`
+}
+
+func retryRequest(method, url string, params map[string]any, header map[string]string) (*http.Response, error) {
 	maxRetries := 5               // 最大重试次数
 	retryDelay := 2 * time.Second // 重试间隔时间
 
 	var err error
 	var req *http.Request
-	var client http.Client
 	var resp *http.Response
+	client := &http.Client{
+		Timeout: 30 * time.Second, // 设置超时时间为 30 秒
+	}
 
-	for i := 0; i < maxRetries; i++ {
-		if method == http.MethodGet {
+	for count := 0; count < maxRetries; count++ {
+		switch method {
+		case http.MethodGet:
 			req, err = http.NewRequest(http.MethodGet, url, nil)
-		} else if method == http.MethodPut {
-			req, err = http.NewRequest(http.MethodPut, url, body)
-		} else if method == http.MethodPost {
-			req, err = http.NewRequest(http.MethodPost, url, body)
-		} else {
-			return nil, fmt.Errorf("不支持的method: %s", method)
+		case http.MethodPut, http.MethodPost:
+			body, erro := json.Marshal(params)
+			if erro != nil {
+				log.Errorf("错误: %+v", erro)
+				return nil, fmt.Errorf("格式化请求体错误: %+v", erro)
+			}
+			bodyBuffer := bytes.NewBuffer(body)
+			contentLength := strconv.Itoa(bodyBuffer.Len())
+			header["Content-Length"] = contentLength
+			req, err = http.NewRequest(method, url, bodyBuffer)
+		default:
+			return nil, fmt.Errorf("不支持的 Method: %s", method)
 		}
 
 		if err != nil {
@@ -55,16 +80,37 @@ func retryRequest(method, url string, body *bytes.Buffer, header map[string]stri
 		}
 
 		resp, err = client.Do(req)
-		if err == nil && resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		if err != nil || resp.StatusCode < 200 || resp.StatusCode >= 400 {
+			// 请求失败，等待重试
+			if resp != nil {
+				defer resp.Body.Close()
+			}
+			log.Warnf("请求 %s 失败, 等待 %ds 后重试", url, retryDelay/1000000000)
+			time.Sleep(retryDelay)
+			continue
+		} else {
+			// 请求成功，返回响应
 			return resp, nil
 		}
-
-		// 请求失败，等待重试
-		log.Warnf("请求 %s 失败, 等待 %ds 后重试: %+v", url, retryDelay/1000000000, err)
-		time.Sleep(retryDelay)
-		resp.Body.Close()
 	}
-	return nil, fmt.Errorf("访问 %s 失败: %+v", url, err)
+
+	switch {
+	case err != nil:
+		return nil, fmt.Errorf("访问 %s 失败: %+v", url, err)
+	case resp != nil && (resp.StatusCode < 200 || resp.StatusCode >= 400):
+		responseBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("访问 %s 失败: %d", url, resp.StatusCode)
+		}
+		respString := strings.TrimSpace(string(responseBody))
+		if respString != "" {
+			return nil, fmt.Errorf("访问 %s 失败: %d\n%s", url, resp.StatusCode, respString)
+		} else {
+			return nil, fmt.Errorf("访问 %s 失败: %d", url, resp.StatusCode)
+		}
+	default:
+		return nil, fmt.Errorf("访问 %s 失败", url)
+	}
 }
 
 func handleMain(config Config) {
@@ -72,29 +118,27 @@ func handleMain(config Config) {
 
 	// 设置请求头
 	header := map[string]string{
-		"X-Auth-Key": config.Key,
+		"X-Auth-Key":   config.Key,
 		"X-Auth-Email": config.Email,
-		"Accept": "application/json",
-		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
+		"Accept":       "application/json",
+		"User-Agent":   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
 	}
 
 	defer func() {
 		content = strings.TrimSpace(content)
 		if config.Telegram.Update && content != "" && config.Telegram.Url != "" && config.Telegram.ID != "" {
 			// 更新IP到指定URL
-			params := map[string]string{
+			params := map[string]any{
 				"chat_id": config.Telegram.ID,
 				"text":    content,
 			}
-			body, err := json.Marshal(params)
-			if err != nil {
-				log.Infof("错误: %+v", err)
-				return
+			header := map[string]string{
+				"Content-Type": "application/json",
+				"User-Agent":   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
 			}
-			_, err = retryRequest("POST", config.Telegram.Url, bytes.NewBuffer(body), header)
+			_, err := retryRequest("POST", config.Telegram.Url, params, header)
 			if err != nil {
-				log.Infof("获取IPV6错误: %+v", err)
-				return
+				log.Errorf("更新 IP 到 Telegram 错误: %+v", err)
 			}
 		}
 	}()
@@ -154,9 +198,25 @@ func handleMain(config Config) {
 			}
 			resp.Body.Close()
 
-			var result map[string]interface{}
-			json.Unmarshal(responseBody, &result)
-			zid := result["result"].([]interface{})[0].(map[string]interface{})["id"].(string)
+			var zoneInfos ZoneInfos
+			err = json.Unmarshal(responseBody, &zoneInfos)
+			if err != nil {
+				log.Errorf("解析 ZoneInfos 错误: %+v", err)
+				content += localIPContent
+				continue
+			}
+			if len(zoneInfos.Result) == 0 {
+				log.Warnf("未找到域名 %s 的 ZoneName", domain)
+				content += localIPContent
+				continue
+			}
+			zid := zoneInfos.Result[0].ID
+			if zid == "" {
+				log.Warnf("未找到域名 %s 的 ZoneID", domain)
+				content += localIPContent
+				continue
+			}
+
 			resp, err = retryRequest("GET", fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records?name=%s.%s", zid, name, domain), nil, header)
 			if err != nil {
 				log.Warnf("获取域名 %s.%s 的 DNS 记录错误: %s", name, domain, err)
@@ -172,28 +232,33 @@ func handleMain(config Config) {
 			}
 			resp.Body.Close()
 
-			var dnsRecords map[string]interface{}
-			json.Unmarshal(responseBody, &dnsRecords)
-			resultList := dnsRecords["result"].([]interface{})
+			var dnsInfos DNSInfos
+			err = json.Unmarshal(responseBody, &dnsInfos)
+			if err != nil {
+				log.Errorf("解析 DNS 记录错误: %+v", err)
+				content += localIPContent
+				continue
+			}
 			rid := ""
 			remortIP := ""
 			proxied := false
-			for _, record := range resultList {
-				if record.(map[string]interface{})["type"].(string) == dnsType {
-					rid = record.(map[string]interface{})["id"].(string)
-					remortIP = record.(map[string]interface{})["content"].(string)
-					proxied = record.(map[string]interface{})["proxied"].(bool)
+			for _, record := range dnsInfos.Result {
+				if record.Type == dnsType {
+					rid = record.ID
+					remortIP = record.RemortIP
+					proxied = record.Proxy
 					break
 				}
 			}
+
 			if rid == "" || remortIP == "" {
 				log.Error("错误: 未获取到 Rid 或 RemortIP")
 				content += localIPContent
 				continue
 			}
+
 			if localIP != remortIP {
 				content += localIPContent
-		
 				params := map[string]interface{}{
 					"id":      zid,
 					"type":    dnsType,
@@ -201,13 +266,8 @@ func handleMain(config Config) {
 					"content": localIP,
 					"proxied": proxied,
 				}
-				body, err := json.Marshal(params)
-				if err != nil {
-					log.Errorf("Json 格式化错误: %+v", err)
-					continue
-				}
 
-				resp, err = retryRequest("PUT", fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s", zid, rid), bytes.NewBuffer(body), header)
+				resp, err = retryRequest("PUT", fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s", zid, rid), params, header)
 				if err != nil {
 					log.Errorf("域名 %s.%s 更新错误: %+v", name, domain, err)
 					continue
